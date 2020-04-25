540a9433fb296455e89b8e81fd0b7335be7ee732
bravemaster619 <bravemaster619@hotmail.com>
2020-04-25 19:12:01 +0800

    feat(page): add home page

---
+++ src/app/pages/home/home.page.spec.ts
@@ -0,0 +1,32 @@
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { IonicModule } from "@ionic/angular";
+
+import { HomePage } from "./home.page";
+import { TranslateModule } from '@ngx-translate/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+describe("HomePage", () => {
+  let component: HomePage;
+  let fixture: ComponentFixture<HomePage>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [HomePage],
+      imports: [
+        HttpClientModule,
+        IonicModule.forRoot(),
+        TranslateModule.forRoot(),
+        RouterModule.forRoot([])
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(HomePage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+});
