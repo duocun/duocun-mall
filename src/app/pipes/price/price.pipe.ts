6d0605f5d83ba5b1d1c2c73a4cc588e8f1011a01
bravemaster619 <bravemaster619@hotmail.com>
2020-04-27 15:02:39 +0800

    feat(pipe): add price pipe

---
+++ src/app/pipes/price/price.pipe.ts
@@ -0,0 +1,27 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+  name: "price",
+  pure: false
+})
+export class PricePipe implements PipeTransform {
+  formatter: any;
+
+  constructor() {
+    this.formatter = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD"
+    });
+  }
+
+  transform(value: any): string {
+    if (typeof value === "object") {
+      return this.transform(value.price);
+    }
+    value = parseFloat(<string>value);
+    if (value < 0 || isNaN(value)) {
+      value = 0;
+    }
+    return this.formatter.format(value);
+  }
+}
