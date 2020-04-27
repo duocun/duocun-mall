a1e5d716f34a633af00adeab924890e77d7ab576
bravemaster619 <bravemaster619@hotmail.com>
2020-04-27 13:25:26 +0800

    chore(route): remove unnecessary routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,18 +6,6 @@ const routes: Routes = [
     path: "",
     loadChildren: () =>
       import("./pages/home/home.module").then((m) => m.HomePageModule)
-  },
-  {
-    path: "cart",
-    loadChildren: () =>
-      import("./pages/cart/cart.module").then((m) => m.CartPageModule)
-  },
-  {
-    path: "my-account",
-    loadChildren: () =>
-      import("./pages/my-account/my-account.module").then(
-        (m) => m.MyAccountPageModule
-      )
   }
 ];
 @NgModule({
