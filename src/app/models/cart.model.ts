0cf34fc104361fa5a1fbeed12a0198ace946514d
bravemaster619 <bravemaster619@hotmail.com>
2020-04-27 17:16:29 +0800

    feat(model): add cart model

---
+++ src/app/models/cart.model.ts
@@ -0,0 +1,21 @@
+
+export interface CartItemInterface {
+  productId: string;
+  productName: string; // product name
+  merchantId: string;
+  merchantName: string;
+  price: number;
+  cost: number;
+  quantity: number;
+}
+
+export interface CartInterface {
+  clientId?: string;
+  clientName?: string;
+  clientPhoneNumber?: string;
+  merchantId: string;
+  merchantName?: string;
+  price?: number;
+  quantity?: number;
+  items: CartItemInterface[];
+}
