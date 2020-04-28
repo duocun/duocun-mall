9a865d9746c72ffea97fb4a432535603a808796e
bravemaster619 <bravemaster619@hotmail.com>
2020-04-28 19:03:10 +0800

    feat(auth): add login flow in home page

---
+++ src/app/models/account.model.ts
@@ -0,0 +1,15 @@
+export interface AccountInterface {
+  _id: string;
+  username?: string;
+  imageurl?: string;
+  sex?: number;
+  type?: string;
+  realm?: string;
+  openId?: string;
+  balance?: number;
+  attributes?: Array<any>;
+  created?: string;
+  modified?: string;
+  phone: string;
+  verified?: boolean;
+}
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from "@angular/core";
 import { CartService } from "src/app/services/cart/cart.service";
 import { CartInterface } from "src/app/models/cart.model";
-
+import { ActivatedRoute, ParamMap } from "@angular/router";
+import { AuthService } from "src/app/services/auth/auth.service";
+import { ApiService } from "src/app/services/api/api.service";
+import { AccountInterface } from "src/app/models/account.model";
+import { AlertController } from "@ionic/angular";
+import { TranslateService } from "@ngx-translate/core";
 @Component({
   selector: "app-home",
   templateUrl: "./home.page.html",
@@ -9,11 +14,92 @@ import { CartInterface } from "src/app/models/cart.model";
 })
 export class HomePage implements OnInit {
   cart: CartInterface;
-  constructor(private cartService: CartService) {}
+  clientId: string;
+  page: string;
+  account: AccountInterface;
+  constructor(
+    private cartService: CartService,
+    private route: ActivatedRoute,
+    private authSvc: AuthService,
+    private api: ApiService,
+    private alert: AlertController,
+    private translator: TranslateService
+  ) {}
 
   ngOnInit() {
+    this.handleQueryParams();
+    this.initCart();
+  }
+
+  handleQueryParams() {
+    this.route.queryParamMap.subscribe((paramAsMap: ParamMap) => {
+      this.clientId = paramAsMap.get("cid");
+      this.page = paramAsMap.get("p");
+      switch (this.page) {
+        case "b":
+          this.handleCreditByWeChat(paramAsMap);
+          break;
+        case "h":
+          this.handleWeChatPay(paramAsMap);
+          break;
+        default:
+          this.initAccount(paramAsMap);
+          break;
+      }
+    });
+  }
+
+  initCart() {
     this.cartService.getCart().subscribe((cart: CartInterface) => {
       this.cart = cart;
+    });
+  }
+
+  initAccount(params: ParamMap) {
+    const tokenId: string = params.get("token");
+    const appCode = params.get("state");
+    if (!tokenId) {
+      this.authSvc.getToken().then((tokenId) => {
+        if (tokenId) {
+          this.getCurrentAccount(tokenId);
+        }
+      });
+    } else {
+      this.getCurrentAccount(tokenId);
+    }
+  }
+
+  getCurrentAccount(tokenId: any) {
+    this.api.get(`Accounts/G/token/${tokenId}`).then((observable) => {
+      observable.subscribe((resp: { code: string; data: AccountInterface }) => {
+        if (resp.code === "success") {
+          this.account = resp.data;
+          this.authSvc.login(tokenId);
+        } else {
+          this.authSvc.logout();
+          this.showAlert("Notice", "Login failed", "OK");
+        }
+      });
+    });
+  }
+
+  handleCreditByWeChat(params: ParamMap) {
+    console.log("handleCreditByWeChat called");
+  }
+
+  handleWeChatPay(params: ParamMap) {
+    console.log("handleWeChatPay called");
+  }
+
+  showAlert(header, message, button) {
+    this.translator.get([header, message, button]).subscribe((dict) => {
+      this.alert
+        .create({
+          header: dict[header],
+          message: dict[message],
+          buttons: [dict[button]]
+        })
+        .then((alert) => alert.present());
     });
   }
 }
