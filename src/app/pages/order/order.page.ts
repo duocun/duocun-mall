2190961c3c029adba48aed22275bb5d8b9b83cde
bravemaster619 <bravemaster619@hotmail.com>
2020-04-28 19:49:03 +0800

    feat(page): add delivery name and phone number to order page

--- src/app/pages/order/order.page.ts
+++ src/app/pages/order/order.page.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from "@angular/core";
 import { CartService } from "src/app/services/cart/cart.service";
 import * as Order from "src/app/models/order.model";
 import { getPictureUrl, ProductInterface } from "src/app/models/product.model";
+import { AccountInterface } from "src/app/models/account.model";
+import { AuthService } from "src/app/services/auth/auth.service";
+import { ApiService } from "src/app/services/api/api.service";
+import { AlertController } from "@ionic/angular";
+import { TranslateService } from "@ngx-translate/core";
 @Component({
   selector: "app-order",
   templateUrl: "./order.page.html",
@@ -11,11 +16,22 @@ export class OrderPage implements OnInit {
   chargeItems;
   order: Order.OrderInterface;
   orderGroups: Array<any>;
+  address: string;
   summary: Order.OrderSummaryInterface;
   notes: string;
-  constructor(private cartSvc: CartService) {}
+  account: AccountInterface;
+  constructor(
+    private cartSvc: CartService,
+    private authSvc: AuthService,
+    private api: ApiService,
+    private alert: AlertController,
+    private translator: TranslateService
+  ) {}
 
   ngOnInit() {
+    this.authSvc.getAccount().subscribe((account) => {
+      this.account = account;
+    });
     this.cartSvc.getCart().subscribe((cart) => {
       this.chargeItems = Order.getChargeItems(cart);
       this.orderGroups = Order.getOrderGroups(cart);
@@ -25,5 +41,17 @@ export class OrderPage implements OnInit {
 
   getPictureUrl(item: ProductInterface) {
     return getPictureUrl(item);
+  }
+
+  showAlert(header, message, button) {
+    this.translator.get([header, message, button]).subscribe((dict) => {
+      this.alert
+        .create({
+          header: dict[header],
+          message: dict[message],
+          buttons: [dict[button]]
+        })
+        .then((alert) => alert.present());
+    });
   }
 }
