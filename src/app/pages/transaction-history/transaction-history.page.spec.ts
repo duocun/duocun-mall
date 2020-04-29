1365df692ce3a9c0f1e4ec35b72c60ce6fde3fd4
bravemaster619 <bravemaster619@hotmail.com>
2020-04-29 21:40:23 +0800

    style: apply prettier

--- src/app/pages/transaction-history/transaction-history.page.spec.ts
+++ src/app/pages/transaction-history/transaction-history.page.spec.ts
@@ -2,12 +2,12 @@ import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { IonicModule } from "@ionic/angular";
 
 import { TransactionHistoryPage } from "./transaction-history.page";
-import { HttpClientModule } from '@angular/common/http';
-import { TranslateModule } from '@ngx-translate/core';
-import { IonicStorageModule } from '@ionic/storage';
-import { MomentPipeModule } from 'src/app/pipes/moment/moment.module';
-import { PricePipeModule } from 'src/app/pipes/price/price.module';
-import { RouterModule } from '@angular/router';
+import { HttpClientModule } from "@angular/common/http";
+import { TranslateModule } from "@ngx-translate/core";
+import { IonicStorageModule } from "@ionic/storage";
+import { MomentPipeModule } from "src/app/pipes/moment/moment.module";
+import { PricePipeModule } from "src/app/pipes/price/price.module";
+import { RouterModule } from "@angular/router";
 
 describe("TransactionHistoryPage", () => {
   let component: TransactionHistoryPage;
