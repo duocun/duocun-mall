0ca44c0468aa0436bdaaf8ada5af33a737957d60
bravemaster619 <bravemaster619@hotmail.com>
2020-04-29 18:39:39 +0800

    test: add test for transaction history

--- src/app/pages/transaction-history/transaction-history.page.spec.ts
+++ src/app/pages/transaction-history/transaction-history.page.spec.ts
@@ -2,6 +2,12 @@ import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { IonicModule } from "@ionic/angular";
 
 import { TransactionHistoryPage } from "./transaction-history.page";
+import { HttpClientModule } from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
+import { IonicStorageModule } from '@ionic/storage';
+import { MomentPipeModule } from 'src/app/pipes/moment/moment.module';
+import { PricePipeModule } from 'src/app/pipes/price/price.module';
+import { RouterModule } from '@angular/router';
 
 describe("TransactionHistoryPage", () => {
   let component: TransactionHistoryPage;
@@ -10,7 +16,15 @@ describe("TransactionHistoryPage", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [TransactionHistoryPage],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        IonicModule.forRoot(),
+        HttpClientModule,
+        TranslateModule.forRoot(),
+        IonicStorageModule.forRoot(),
+        MomentPipeModule,
+        PricePipeModule,
+        RouterModule.forRoot([])
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(TransactionHistoryPage);
