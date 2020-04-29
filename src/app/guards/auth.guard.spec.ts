851c1c9b391350a98c98aaaf15c0464cf48418e9
bravemaster619 <bravemaster619@hotmail.com>
2020-04-29 14:50:07 +0800

    test(guard): import necessary modules in auth guard spec

--- src/app/guards/auth.guard.spec.ts
+++ src/app/guards/auth.guard.spec.ts
@@ -2,11 +2,18 @@ import { TestBed, async, inject } from "@angular/core/testing";
 import { IonicStorageModule } from "@ionic/storage";
 import { AuthGuard } from "./auth.guard";
 import { RouterModule } from "@angular/router";
+import { TranslateModule } from "@ngx-translate/core";
+import { HttpClientModule } from "@angular/common/http";
 
 describe("AuthGuard", () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [IonicStorageModule.forRoot(), RouterModule.forRoot([])],
+      imports: [
+        IonicStorageModule.forRoot(),
+        RouterModule.forRoot([]),
+        TranslateModule.forRoot(),
+        HttpClientModule
+      ],
       providers: [AuthGuard]
     });
   });
