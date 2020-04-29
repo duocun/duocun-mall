96cf50f66a1068b27993eec02eb67bf892796f2d
bravemaster619 <bravemaster619@hotmail.com>
2020-04-29 14:45:15 +0800

    feat(guard): show alert when not logged in

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -8,12 +8,19 @@ import {
 import { Observable } from "rxjs";
 import { AuthService } from "src/app/services/auth/auth.service";
 import { Router } from "@angular/router";
+import { AlertController } from "@ionic/angular";
+import { TranslateService } from "@ngx-translate/core";
 
 @Injectable({
   providedIn: "root"
 })
 export class AuthGuard implements CanActivate {
-  constructor(public auth: AuthService, private router: Router) {}
+  constructor(
+    public auth: AuthService,
+    private router: Router,
+    private alert: AlertController,
+    private translator: TranslateService
+  ) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
@@ -28,12 +35,29 @@ export class AuthGuard implements CanActivate {
         if (isLoggedIn) {
           observer.next(true);
         } else {
+          this.showAlert();
           this.router.navigate(["/"], {
             queryParams: { returnUrl: state.url }
           });
           observer.next(false);
         }
       });
+    });
+  }
+  showAlert() {
+    const header = "Notice";
+    const message = "You are not logged in";
+    const button = "OK";
+    this.translator.get([header, message, button]).subscribe((dict) => {
+      this.alert
+        .create({
+          header: dict[header],
+          message: dict[message],
+          buttons: [dict[button]]
+        })
+        .then((alert) => {
+          alert.present();
+        });
     });
   }
 }
