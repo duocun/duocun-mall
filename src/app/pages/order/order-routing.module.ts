f07fc6d33375db584de9d8f50a1263a4815c0909
bravemaster619 <bravemaster619@hotmail.com>
2020-04-29 14:46:16 +0800

    feat(auth): apply auth guard to my account and order pages

--- src/app/pages/order/order-routing.module.ts
+++ src/app/pages/order/order-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 import { OrderPage } from "./order.page";
+import { AuthGuard } from "src/app/guards/auth.guard";
 
 const routes: Routes = [
   {
     path: "",
-    component: OrderPage
+    component: OrderPage,
+    canActivate: [AuthGuard]
   }
 ];
 
