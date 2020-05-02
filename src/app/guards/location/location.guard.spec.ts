21423b1d7ee2df549d12fe6adb209ca6338beee1
bravemaster619 <bravemaster619@hotmail.com>
2020-05-02 14:01:59 +0800

    feat(guard): add location guard
    It will redirect to account setting page when location is not set

---
+++ src/app/guards/location/location.guard.spec.ts
@@ -0,0 +1,24 @@
+import { TestBed, async, inject } from "@angular/core/testing";
+import { IonicStorageModule } from "@ionic/storage";
+import { LocationGuard } from "./location.guard";
+import { RouterModule } from "@angular/router";
+import { TranslateModule } from "@ngx-translate/core";
+import { HttpClientModule } from "@angular/common/http";
+
+describe("LocationGuard", () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        IonicStorageModule.forRoot(),
+        RouterModule.forRoot([]),
+        TranslateModule.forRoot(),
+        HttpClientModule
+      ],
+      providers: [LocationGuard]
+    });
+  });
+
+  it("should create a location guard", inject([LocationGuard], (guard: LocationGuard) => {
+    expect(guard).toBeTruthy();
+  }));
+});
---
+++ src/app/guards/location/location.guard.ts
@@ -0,0 +1,60 @@
+import { Injectable } from "@angular/core";
+import {
+  CanActivate,
+  Router,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree
+} from "@angular/router";
+import { LocationService } from "src/app/services/location/location.service";
+import { AlertController } from "@ionic/angular";
+import { Observable } from "rxjs";
+import { TranslateService } from "@ngx-translate/core";
+
+@Injectable({
+  providedIn: "root"
+})
+export class LocationGuard implements CanActivate {
+  constructor(
+    private locSvc: LocationService,
+    private alert: AlertController,
+    private router: Router,
+    private translator: TranslateService
+  ) {}
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return new Observable((observer) => {
+      this.locSvc.getLocation().subscribe((location) => {
+        if (location) {
+          observer.next(true);
+        } else {
+          this.showAlert();
+          this.router.navigate(["/tabs/my-account/account-setting"]);
+          observer.next(false);
+        }
+      });
+    });
+  }
+  showAlert() {
+    const header = "Notice";
+    const message = "Please select delivery address";
+    const button = "OK";
+    this.translator.get([header, message, button]).subscribe((dict) => {
+      this.alert
+        .create({
+          header: dict[header],
+          message: dict[message],
+          buttons: [dict[button]]
+        })
+        .then((alert) => {
+          alert.present();
+        });
+    });
+  }
+}
