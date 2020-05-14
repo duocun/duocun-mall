5f7436bbdcc6149c2fcd4fe82e80a850e520bffc
bravemaster619 <bravemaster619@hotmail.com>
2020-05-14 15:08:43 +0800

    revert(route): remove login route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,6 @@ const routes: Routes = [
     path: "",
     loadChildren: () =>
       import("./pages/home/home.module").then((m) => m.HomePageModule)
-  },
-  {
-    path: "login",
-    loadChildren: () =>
-      import("./pages/login/login.module").then((m) => m.LoginPageModule)
   }
 ];
 @NgModule({
