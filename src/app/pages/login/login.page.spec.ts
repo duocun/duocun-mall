f6d7df4d0e7d4f15a566f32a1ec9ad0271f9fdf7
bravemaster619 <bravemaster619@hotmail.com>
2020-05-15 19:00:45 +0800

    test: fix test for login module

--- src/app/pages/login/login.page.spec.ts
+++ src/app/pages/login/login.page.spec.ts
@@ -2,6 +2,11 @@ import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { IonicModule } from "@ionic/angular";
 
 import { LoginPage } from "./login.page";
+import { HttpClientModule } from '@angular/common/http';
+import { IonicStorageModule } from '@ionic/storage';
+import { TranslateModule } from '@ngx-translate/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 describe("LoginPage", () => {
   let component: LoginPage;
@@ -10,7 +15,14 @@ describe("LoginPage", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [LoginPage],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        IonicModule.forRoot(),
+        HttpClientModule,
+        IonicStorageModule.forRoot(),
+        TranslateModule.forRoot(),
+        FormsModule,
+        RouterModule.forRoot([])
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginPage);
