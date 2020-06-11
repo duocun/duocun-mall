a21ffeb9028e891e64d84efe044a7d9105d23250
bravemaster619 <bravemaster619@hotmail.com>
2020-06-12 00:28:19 +0800

    fix(page): fix infinite loop in order page when cart is empty

--- src/app/pages/order/order.page.ts
+++ src/app/pages/order/order.page.ts
@@ -118,7 +118,7 @@ export class OrderPage implements OnInit, OnDestroy {
             this.cartSubscription = this.cartSvc
               .getCart()
               .pipe(
-                filter((cart) => !!cart && cart.items.length > 0),
+                filter((cart) => !!cart),
                 takeUntil(this.unsubscribe$)
               )
               .subscribe(async (cart) => {
@@ -138,6 +138,7 @@ export class OrderPage implements OnInit, OnDestroy {
                 this.cartItemGroups = Order.getCartItemGroups(cart);
                 this.orders = [];
                 if (!this.cartItemGroups.length) {
+                  await this.dismissLoading();
                   return this.router.navigate(["/tabs/browse"], {
                     replaceUrl: true
                   });
