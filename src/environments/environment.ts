c8f3365858896df07967cd3aeed06ed1418d3376
zlkca <likzhang@gmail.com>
2020-08-20 01:18:38 -0400

    update api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  api: "http://localhost:8000/api",
+  api: "https://api.duocun.ca/v1dev",
   storageKey: {
     auth: "DUOCUN-AUTH-TOKEN",
     cart: "DUOCUN-CART",
