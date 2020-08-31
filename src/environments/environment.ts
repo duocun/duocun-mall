c3d25757c264a9fc6c36ee1105cab7252c5f6cfb
zlkca <likzhang@gmail.com>
2020-08-31 12:39:13 -0400

    remove credentials in environment files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,15 +12,14 @@ export const environment = {
     lang: "DUOCUN-I18N"
   },
   media: "https://d27ftifr2ocsa6.cloudfront.net/media/",
-  stripe: "pk_test_FsGoacByMF8lGNcVKveNZFSy00ATWTilJp",
+  stripe: "",
   defaultLang: "en",
-  gmap: "AIzaSyCEd6D6vc9K-YzMH-QtQWRSs5HZkLKSWyk",
+  gmap: "",
   timezone: "Asia/Shanghai",
   monerisMode: "prod",
-  monerisHTProfileId: "ht4E3Z1H87Y3327",
-  googleAuthClientId:
-    "53858676843-kjkp8dt8e01tktpc8pimd3vtlvkcre81.apps.googleusercontent.com",
-  facebookAppId: "3115867368492639",
+  monerisHTProfileId: "",
+  googleAuthClientId:"",
+  facebookAppId: "",
   socket: "http://localhost:8000"
 };
 
