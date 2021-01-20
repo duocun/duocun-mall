82fb727727cee2d0de95098d2f931d8cd2c7ffb2
terminator <terminator940120@yahoo.co.jp>
2021-01-20 16:14:14 +0800

    update prod environment values

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: "http://localhost:8001/v1", // "https://api.duocun.ca/v1dev",
+  api: "https://api.duocun.ca/v1",
   storageKey: {
     auth: "DUOCUN-AUTH-TOKEN",
     cart: "DUOCUN-CART",
@@ -8,14 +8,15 @@ export const environment = {
     lang: "DUOCUN-I18N"
   },
   media: "https://d27ftifr2ocsa6.cloudfront.net/media/",
-  stripe: "",
+  stripe: "pk_live_vV7H1l4X9k2r8uf6JDf9lGsb",
   defaultLang: "zh",
-  GOOGLE_MAP_KEY: "",
+  GOOGLE_MAP_KEY: "AIzaSyCpOl3ou-sgPg5vfHQO0jWXkS1gJ4SDg8M",
   timezone: "America/Toronto",
   monerisMode: "prod",
-  monerisHTProfileId: "",
-  googleAuthClientId: "",
-  facebookAppId: "",
-  socket: "http://localhost:8001", // "https://api.duocun.ca/v1dev", //
+  monerisHTProfileId: "htV6VVM9NH24SXX",
+  googleAuthClientId:
+    "53858676843-kjkp8dt8e01tktpc8pimd3vtlvkcre81.apps.googleusercontent.com",
+  facebookAppId: "3115867368492639",
+  socket: "https://api.duocun.ca",
   svcPath: "/v1"
 };
